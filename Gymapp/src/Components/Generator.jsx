c69f84d98de4d850927c66f5860a2caa166fa39a
Keyush-Kush <kyus100shrestha@gmai.com>
2024-10-06 11:52:59 +0545

    fixing the grid

--- Gymapp/src/Components/Generator.jsx
+++ Gymapp/src/Components/Generator.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import SectionWrapper from './SectionWrapper'
+import { WORKOUTS } from '../utils/swoldier'
 
 function Header(props){
   const {index,title,description}=props
   return(
     <div className='flex flex-col gap-4'>
-      <div className='flex items-center justify-center gap-2'>
-        <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
-        <h4 className='text-xl sm:text-2xl md:text-3xl '>{title}</h4>
-      </div>
-      <p className='test-sm sm:text-base x-auto'>{description}</p>
-    </div>
+            <div className='flex items-center justify-center gap-2'>
+                <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+                <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
+            </div>
+            <p className='text-sm sm:text-base mx-auto'>{description}</p>
+        </div>
   )
 }
 
@@ -18,8 +19,18 @@ function Generator() {
   return (
     <SectionWrapper header={"generate your workout"} title=
     {['It\'s','Huge','o\'clock']}>
-      <Header index={'01'} title={"Warm Up"} description={"5 minutes to get the blood flowing"}/>
-      
+      <Header index={'01'} title={"Pick POSITION"} description={"SELECT to get the blood flowing"}/>
+      <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+
+    
+      {Object.keys(WORKOUTS).map((type,typeIndex)=>{
+        return(
+          <button key={typeIndex}>
+            <p>{type}</p>
+          </button>
+        )
+      })}
+        </div>
     </SectionWrapper>
   )
 }
