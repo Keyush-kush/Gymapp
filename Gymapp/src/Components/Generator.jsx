74a40cc35ad6004518d90aa002a51e374b8398d4
Keyush-Kush <kyus100shrestha@gmai.com>
2024-10-06 10:14:58 +0545

    New Section added

--- Gymapp/src/Components/Generator.jsx
+++ Gymapp/src/Components/Generator.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SectionWrapper from './SectionWrapper'
+
+function Header(props){
+  const {index,title,description}=props
+  return(
+    <div className='flex flex-col gap-4'>
+      <div className='flex items-center justify-center gap-2'>
+        <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+        <h4 className='text-xl sm:text-2xl md:text-3xl '>{title}</h4>
+      </div>
+      <p className='test-sm sm:text-base x-auto'>{description}</p>
+    </div>
+  )
+}
 
 function Generator() {
   return (
-    <div>Generator</div>
+    <SectionWrapper header={"generate your workout"} title=
+    {['It\'s','Huge','o\'clock']}>
+      <Header index={'01'} title={"Warm Up"} description={"5 minutes to get the blood flowing"}/>
+      
+    </SectionWrapper>
   )
 }
 
--- Gymapp/src/Components/Hero.jsx
+++ Gymapp/src/Components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
         <div className='flex flex-col gap-4'>
     <p>IT'S TIME TO GET</p>
-    <h1 className='uppercase text-4xl font-semibold sm:text-5xl md:text-6xl lg:text-7xl'>work<span className='text-blue-400 font-medium'>extreme</span></h1>
+    <h1 className='uppercase text-4xl font-semibold sm:text-5xl md:text-6xl lg:text-7xl'>work<span className='text-blue-400 font-medium glow-on-hover'>extreme</span></h1>
     </div>
     <p className='text-lg sm:text-xl md:text-2xl lg:text-3xl'>Lorem ipsum dolor sit amet consectetur, 
         adipisicing elit. Omnis <span className='text-blue-400 font-medium'>reprehenderit</span> similique 
@@ -14,7 +14,11 @@ function Hero() {
         mollitia dolores cum atque 
         vel praesentium.</p>
 
-        <button className='px-8 py-3 bg-black-400 text-white uppercase font-semibold rounded-lg blueShadow duration-300'><p>Accept & Begin</p></button>
+        <button 
+  className='px-8 py-3 bg-gray-800 text-white  uppercase font-semibold rounded-lg blueShadow  duration-300  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+>
+  <p>Accept & Begin</p>
+</button>
   </div>
   )
 }
