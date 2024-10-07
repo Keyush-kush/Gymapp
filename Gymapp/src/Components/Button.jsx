0e764f3f510b241ce669bbd87710c7881718a1ab
Keyush-Kush <kyus100shrestha@gmai.com>
2024-10-07 14:22:38 +0545

    😓😓😓

--- Gymapp/src/Components/Button.jsx
+++ Gymapp/src/Components/Button.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
-function Button(props) {
-    const {text,func} = props;
-  return (
-    <button onClick={func}
-    className='px-8 mx-auto py-3 bg-gray-800 text-white  uppercase font-semibold rounded-lg blueShadow  duration-300  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
-  >
-    <p>{text}</p>
-  </button>
-  )
+export default function Button(props) {
+    const { text, func } = props
+    return (
+        <button onClick={func} className='px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+            <p>{text}</p>
+        </button>
+    )
 }
-
-export default Button
--- Gymapp/src/Components/Workout.jsx
+++ Gymapp/src/Components/Workout.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import ExerciseCard from './ExerciseCard'
 
-export default function SectionWrapper(props) {
-    const { children, header, title, id } = props
+export default function Workout(props) {
+    const { workout } = props
     return (
-        <section id={id} className='min-h-screen flex flex-col gap-10'>
-            <div className='bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center p-4'>
-                <p className='uppercase font-medium'>{header}</p>
-                <h2 className='font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-6xl'>{title[0]} <span className='uppercase text-blue-400'>{title[1]}</span> {title[2]}</h2>
+        <SectionWrapper id={'workout'} header={"welcome to"} title={['The', 'DANGER', 'zone']}>
+            <div className='flex flex-col gap-4'>
+                {workout.map((exercise, i) => {
+                    return (
+                        <ExerciseCard i={i} exercise={exercise} key={i} />
+                    )
+                })}
             </div>
-            <div className='max-w-[800px] w-full flex flex-col mx-auto gap-10 p-4 '>
-                {children}
-            </div>
-        </section>
+        </SectionWrapper>
     )
 }
